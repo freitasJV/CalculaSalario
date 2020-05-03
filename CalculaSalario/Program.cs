d793188168abbbe2c3761c62823c981016b9d830
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 16:02:21 -0300

    Adicionando tratamento de excessões

--- CalculaSalario/Program.cs
+++ CalculaSalario/Program.cs
@@ -7,20 +7,31 @@ namespace CalculaSalario
     {
         static void Main(string[] args)
         {
-            int numeroFunc, horasTrabalhadas;
-            double valorHora, salario;
+            try
+            {
+                int numeroFunc, horasTrabalhadas;
+                double valorHora, salario;
 
-            Console.WriteLine("Digite o número do funcionário:");
-            numeroFunc = int.Parse(Console.ReadLine());  
-            Console.WriteLine("Digite o número de horas trabalhadas:");
-            horasTrabalhadas = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o valor da hora trabalhada:");
-            valorHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                Console.WriteLine("Digite o número do funcionário:");
+                numeroFunc = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o número de horas trabalhadas:");
+                horasTrabalhadas = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o valor da hora trabalhada:");
+                valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            salario = horasTrabalhadas * valorHora;
+                salario = horasTrabalhadas * valorHora;
 
-            Console.WriteLine($"NUMBER = {numeroFunc}");
-            Console.WriteLine($"SALARY = U$ {salario.ToString("F2",CultureInfo.InvariantCulture)}");
+                Console.WriteLine($"NUMBER = {numeroFunc}");
+                Console.WriteLine($"SALARY = U$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+            }
         }
     }
 }
